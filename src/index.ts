98748ebedaf89296638bc9bb7a7a669f2da13f35
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-03-29 12:02:59 +0530

    added the migrations

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username: string, password: string, firstname: string, lastname: string, email: string) {
+
+}
