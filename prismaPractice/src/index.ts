d4d5933dff2a94ceb690b67d3e9720b7364f10fd
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-03-29 13:11:03 +0530

    added createUser and getUsers functions

--- prismaPractice/src/index.ts
+++ prismaPractice/src/index.ts
@@ -3,5 +3,27 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function insertUser(username: string, password: string, firstname: string, lastname: string, email: string) {
-
+    const response = await prisma.user.create({
+        data: {
+            username,
+            password,
+            firstname,
+            lastname,
+            email
+        }
+    })
+    console.log(response)
 }
+
+// insertUser("c", "c", "c", "c", "c",);
+
+async function getUsers(userid: number) {
+    const response = await prisma.user.findMany({
+        where: {
+            id : userid
+        }
+    })
+    console.log(response)
+}
+
+// getUsers(3)
